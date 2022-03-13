575a13495cb29b3351852347b71dc2003de0f8b4
Daniel Fernandez <danyfernandeza@gmail.com>
2022-03-13 14:50:18 +0100

    feat: Add array DS

---
+++ data-structures/array/README.md
@@ -0,0 +1,20 @@
+# Arrays
+
+The array data structure is use to store a collection of elements and provides a very convenient way of accessing these elements `array[]`. Arrays have a fixed size thought, meaning that adding and removing element from the beginning or the middle of the array could be an expensive operation as the elements need to be shifted over.
+
+> How exactly arrays are implemented depends on the specific JavaScript interpreter or VM. The following explanation assumes we are using the V8 JavaScript engine.
+
+In JavaScript, arrays are in fact dynamic meaning that the array will automatically resize. When adding an extra item to an existing array, for instance by calling `Array.prototype.push()`, the engine will allocate a new, bigger array, copy the existing elements over, and then add the new value.
+
+_The above won't be true for "sparse" arrays, (arrays are arrays with a lot of empty spots in the middle) in which case the array will be treated by the engine as a hash map._
+
+One last important thing to mention is that JavaScript array implementation defers from other languages in the sense that you can create an array that contains multiple different data types.
+
+## Time complexity analysis
+
+| Operation           | Time complexity |
+| ------------------- | --------------- |
+| Access i-th element | O(1)            |
+| Traverse            | O(N)            |
+| Insert              | O(N)            |
+| Delete              | O(N)            |
---
+++ data-structures/array/array.test.ts
@@ -0,0 +1,74 @@
+describe('Array', () => {
+  describe('Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees.', () => {
+    const matrix = [
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10],
+      [11, 12, 13, 14, 15],
+      [16, 17, 18, 19, 20],
+      [21, 22, 23, 24, 25],
+    ];
+
+    const rotatedMatrix = [
+      [21, 16, 11, 6, 1],
+      [22, 17, 12, 7, 2],
+      [23, 18, 13, 8, 3],
+      [24, 19, 14, 9, 4],
+      [25, 20, 15, 10, 5],
+    ];
+
+    /**
+     * Time complexity: O(n2)
+     * Space complexity: O(n2)
+     */
+    test('Solution 1', () => {
+      const n = matrix.length;
+      const result: number[][] = [];
+
+      for (let i = 0; i < n; i++) {
+        for (let j = n - 1; j >= 0; j--) {
+          if (!result[i]) {
+            result[i] = [matrix[j][i]];
+          } else {
+            result[i].push(matrix[j][i]);
+          }
+        }
+      }
+
+      expect(result).toEqual(rotatedMatrix);
+    });
+
+    /**
+     * Time complexity: O(n2)
+     * Space complexity: O(1)
+     */
+    test('Solution 2 (in place)', () => {
+      const n = matrix.length;
+
+      for (let layer = 0; layer < n / 2; layer++) {
+        const first = layer;
+        const last = n - 1 - layer;
+
+        for (let i = first; i < last; i++) {
+          const offset = i - first;
+
+          // Start swapping
+          const top = matrix[first][i];
+
+          // left goes to top
+          matrix[first][i] = matrix[last - offset][first];
+
+          // bottom goes to left
+          matrix[last - offset][first] = matrix[last][last - offset];
+
+          // right goes to bottom
+          matrix[last][last - offset] = matrix[i][last];
+
+          // top goes to right
+          matrix[i][last] = top;
+        }
+      }
+
+      expect(matrix).toEqual(rotatedMatrix);
+    });
+  });
+});
