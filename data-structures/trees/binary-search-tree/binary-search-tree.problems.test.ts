473d4c703402603b791c3ccadc84085998b36643
Daniel Fernandez Arias <danyfernandeza@gmail.com>
2024-01-07 12:23:46 -0600

    feat: Add binary tree problem #4

--- data-structures/trees/binary-search-tree/binary-search-tree.problems.test.ts
+++ data-structures/trees/binary-search-tree/binary-search-tree.problems.test.ts
@@ -1,9 +1,9 @@
-import { BinarySearchTree } from './binary-search-tree';
+import { BinarySearchTree } from "./binary-search-tree";
 
 // You can find the problem descriptions in ./README.md
-describe('BinarySearchTree', () => {
-  describe('Problem 1', () => {
-    test('Solution 1', () => {
+describe("BinarySearchTree", () => {
+  describe("Problem 1", () => {
+    test("Solution 1", () => {
       expect(true).toBe(true);
     });
   });
--- data-structures/trees/tree/tree.problems.test.ts
+++ data-structures/trees/tree/tree.problems.test.ts
@@ -1,9 +1,9 @@
-import { Node } from '../binary-search-tree/node';
-import { Queue } from '../../queue/queue';
+import { Node } from "../binary-search-tree/node";
+import { Queue } from "../../queue/queue";
 
 // You can find the problem descriptions in ./README.md
-describe('Tree', () => {
-  describe('Problem 1', () => {
+describe("Tree", () => {
+  describe("Problem 1", () => {
     const root = new Node<number>(1);
 
     root.leftNode = new Node(2);
@@ -31,7 +31,7 @@ describe('Tree', () => {
      * Time complexity: O(n)
      * Space complexity: O(h) where h is the height of the tree
      */
-    test('Solution 1', () => {
+    test("Solution 1", () => {
       // Using pre-order traversal
       function traverse(
         node: Node | null,
@@ -61,7 +61,7 @@ describe('Tree', () => {
      * Time complexity: O(n + m)
      * Space complexity: O(n)
      */
-    test('Solution 2', () => {
+    test("Solution 2", () => {
       // BFS solution
 
       const queue = new Queue<Node>();
@@ -98,7 +98,7 @@ describe('Tree', () => {
     });
   });
 
-  describe('Problem 2', () => {
+  describe("Problem 2", () => {
     const root = new Node<number>(1);
 
     root.leftNode = new Node(2);
@@ -119,7 +119,7 @@ describe('Tree', () => {
      * Time complexity: O(n)
      * Space complexity: O(h) where h is the height of the tree ?
      */
-    test('Solution 1', () => {
+    test("Solution 1", () => {
       // pre-order traverse
       function traverse(node: Node | null, levels: number): number {
         if (node === null) {
@@ -141,7 +141,7 @@ describe('Tree', () => {
     });
   });
 
-  describe('Problem 3', () => {
+  describe("Problem 3", () => {
     class ConnectedNode<T = number> {
       leftNode: ConnectedNode<T> | null = null;
       rightNode: ConnectedNode<T> | null = null;
@@ -157,22 +157,22 @@ describe('Tree', () => {
      * Time complexity:
      * Space complexity:
      */
-    test('Solution 1', () => {
+    test("Solution 1", () => {
       //      A
       //     / \
       //    B   C
       //   / \   \
       //  D   E   F
 
-      const root = new ConnectedNode<string>('A');
+      const root = new ConnectedNode<string>("A");
 
-      root.leftNode = new ConnectedNode<string>('B');
-      root.rightNode = new ConnectedNode<string>('C');
+      root.leftNode = new ConnectedNode<string>("B");
+      root.rightNode = new ConnectedNode<string>("C");
 
-      root.leftNode.leftNode = new ConnectedNode<string>('D');
-      root.leftNode.rightNode = new ConnectedNode<string>('E');
+      root.leftNode.leftNode = new ConnectedNode<string>("D");
+      root.leftNode.rightNode = new ConnectedNode<string>("E");
 
-      root.rightNode.rightNode = new ConnectedNode<string>('F');
+      root.rightNode.rightNode = new ConnectedNode<string>("F");
 
       //     Output Tree
       //      A--->null
@@ -202,13 +202,70 @@ describe('Tree', () => {
 
       expect(root.nextRight).toBeNull();
 
-      expect(root.leftNode.nextRight?.value).toBe('C');
+      expect(root.leftNode.nextRight?.value).toBe("C");
       expect(root.rightNode.nextRight).toBeNull();
 
-      expect(root.leftNode.leftNode.nextRight?.value).toBe('E');
-      expect(root.leftNode.rightNode.nextRight?.value).toBe('F');
+      expect(root.leftNode.leftNode.nextRight?.value).toBe("E");
+      expect(root.leftNode.rightNode.nextRight?.value).toBe("F");
 
       expect(root.rightNode.rightNode.nextRight).toBeNull();
+    });
+  });
+
+  describe("Problem 4", () => {
+    const root = new Node<number>(1);
+
+    root.leftNode = new Node(2);
+    root.rightNode = new Node(3);
+
+    root.leftNode.leftNode = new Node(4);
+    root.leftNode.rightNode = new Node(5);
+
+    root.rightNode.leftNode = new Node(6);
+    root.rightNode.rightNode = new Node(7);
+
+    test("Solution 1", () => {
+      const str = "1,2,4,#,#,5,#,#,3,6,#,#,7,#,#";
+
+      function serialize(node: Node<number> | null, str: string = "") {
+        if (node === null) {
+          if (str) {
+            return `${str},#`;
+          } else {
+            return "#";
+          }
+        }
+
+        if (str) {
+          str = `${str},${node.value}`;
+        } else {
+          str = `${node.value}`;
+        }
+
+        str = serialize(node.leftNode, str) + "," + serialize(node.rightNode);
+
+        return str;
+      }
+
+      function deserialize(root: Node<number> | null, str: string[]) {
+        const val = str.shift();
+
+        if (!val || val === "#") {
+          return null;
+        }
+
+        root = new Node<number>(Number(val));
+
+        root.leftNode = deserialize(root.leftNode, str);
+        root.rightNode = deserialize(root.rightNode, str);
+
+        return root;
+      }
+
+      expect(serialize(root)).toBe(str);
+      expect(serialize(deserialize(new Node<number>(-1), str.split(",")))).toBe(
+        str
+      );
     });
   });
 });
